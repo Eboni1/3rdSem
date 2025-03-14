9b0e6d834a44bf04354ec948f3ca8190718b7529
walts731 <142887725+walts731@users.noreply.github.com>
2025-03-14 12:15:18 +0800

    logout feature

--- super_admin_dashboard.php
+++ super_admin_dashboard.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (!isset($_SESSION["user_id"]) || $_SESSION["role"] !== "super_admin") {
-    header("Location: index.html"); // Redirect if not Super Admin
+    header("Location: index.php"); // Redirect if not Super Admin
     exit;
 }
 ?>
